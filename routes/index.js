7b0ef4680c7f331d315109050810d4d8566af19b
cchenlll <530357478@qq.com>
2018-05-19 20:30:32 +0800

    modify url

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var http = require('http');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, respone, next) {
 
 	// finder('/Users/cchenlll/Desktop/node-pachong/images', (err, files) => {
 	// 	if (err) {
@@ -15,12 +16,31 @@ router.get('/', function(req, res, next) {
 	// 		data: files
 	// 	});
 	// });
-	var buf = fs.readFileSync('./public/urls0.json', (error) => console.log(error));
+	// var buf = fs.readFileSync('./public/urls0.json', (error) => console.log(error));
 	// console.log('---', buf.toString());
 	// res.send(buf.toString());
-	res.render('index', {
-		data: JSON.parse(buf.toString())
-	})
+	// res.render('index', {
+	// 	data: JSON.parse(buf.toString())
+	// })
+	var urlStr = 'http://39.106.30.113:8888/listpic'
+	http.get(urlStr, function(res) {
+		var html = '';
+		console.log('状态码：', res.statusCode);
+		res.setEncoding('UTF-8');
+		res.on('data', function(chunk) {
+			html += chunk;
+		});
+		res.on('end', () => {
+			var dataObj = JSON.parse(html);
+			console.log(dataObj);
+			respone.render('index', {
+				data: dataObj
+			});
+		});
+	}).on('error', function(err) {
+		console.log(err);
+	});
+
 });
 
 function finder(path, callback) {
